34d8d50c4c4973679cde54598565020380bd12a4
bostela <159790066+bostela@users.noreply.github.com>
2024-04-14 14:14:10 +0300

    Update notebook.py
    Šis kodas aprašo du Flask maršrutus, kurie atsako į HTTP POST užklausas

--- notebook.py
+++ notebook.py
@@ -17,3 +17,16 @@ def get_users():
 def get_notes():
     return render_template('notes.html', notes=notes)
 
+# Maršrutas vartotojams (POST)
+@app.route('/users', methods=['POST'])
+def add_user():
+    new_user = request.form['username']
+    users.append(new_user)
+    return redirect(url_for('get_users'))
+
+# Maršrutas užrašams (POST)
+@app.route('/notes', methods=['POST'])
+def add_note():
+    new_note = request.form['note']
+    notes.append(new_note)
+    return redirect(url_for('get_notes'))
